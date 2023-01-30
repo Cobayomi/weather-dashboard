9d16485595daf11b994f02a528579d854b1918ff
capricia <capriciaw16@gmail.com>
2023-01-30 02:06:45 -0600

    upd key

--- assests/script.js
+++ assests/script.js
@@ -1,5 +1,5 @@
 // Global variables
-var APIKey = 'dd3c8bf55d91ab4371e0acb375048f53'
+var APIKey = '86f4cd96d013d52d553577ecdc931bd8'
 
 
 fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={dd3c8bf55d91ab4371e0acb375048f53}')
