f5f01ef24d0b23b8b66b239fb54551f8c52a2bc9
capricia <capriciaw16@gmail.com>
2023-03-20 01:11:03 -0500

    upd

--- assests/script.js
+++ assests/script.js
@@ -1,5 +1,25 @@
 // Global variables
-var APIKey = '86f4cd96d013d52d553577ecdc931bd8'
+var key = "86f4cd96d013d52d553577ecdc931bd8"
+var cityName = document.querySelector(".name");
+var currentCity = document.querySelector(".currentcity");
+var  futureweather = document.querySelector(".future");
+var fiveForecast = document.querySelector(".fiveday");
+
+
+const temp = document.getElementById("temp");
+
+let currentCity = "";
+let currentTemp = "";
+let currentWing = "";
+let currentHumidity = "";
+
+function getDataTime() {
+    let 
+}
+
+
+
+
 
 
 fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={dd3c8bf55d91ab4371e0acb375048f53}')
